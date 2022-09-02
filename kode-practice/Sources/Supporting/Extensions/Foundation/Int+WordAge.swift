e64025e243192fcb44b85b443ad8fe323047bbeb
Mikhail Chaus <setting.chaus@gmail.com>
2022-09-02 10:08:41 +0200

    Переименование цифр в зависимости от контекста

--- kode-practice/Sources/Supporting/Extensions/Foundation/Int+WordAge.swift
+++ kode-practice/Sources/Supporting/Extensions/Foundation/Int+WordAge.swift
@@ -2,10 +2,10 @@ import Foundation
 
 extension Int {
     func ruWordAge() -> String {
-        guard self != .one else { return "год" }
+        guard self != 1 else { return "год" }
         
         var moreThanTwenty: String {
-            if self % 10 == .one {
+            if self % 10 == 1 {
                 return "год"
             } else if (1...4).contains(self % 10) {
                 return  "года"
